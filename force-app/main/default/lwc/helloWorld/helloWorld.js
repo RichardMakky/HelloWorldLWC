09775defc6e62f754a9f4280ed44644a12431aaa
RichardMakky <102604654+RichardMakky@users.noreply.github.com>
2023-06-14 13:30:15 +0200

    Update helloWorld.js

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,7 +1,18 @@
 import { LightningElement } from "lwc";
+import testMethod from '@salesforce/apex/LWCController.test'
 export default class HelloWorld extends LightningElement {
   greeting = "World";
   changeHandler(event) {
     this.greeting = event.target.value;
+  }
+
+  connectedCallback() {
+    testMethod({testParam: this.greeting})
+    .then(result => {
+      console.log(result);
+    })
+    .catch(error => {
+      console.log(error);
+    })
   }
 }
